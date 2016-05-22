d19a864586f84d47058dc6c66ae0a578c4b0f047
Kelvin D. Meeks <kmeeks@intltechventures.com>
2016-05-22 02:08:42 -0700

    updated xyplot.R comments

--- src/xyplot.R
+++ src/xyplot.R
@@ -1,5 +1,10 @@
 # Lattice Graphs
+# 
 # http://www.statmethods.net/advgraphs/trellis.html
+# http://master.bioconductor.org/help/course-materials/2008/advanced_R/latticeLab.pdf
+# https://manning-content.s3.amazonaws.com/download/e/d13d6ca-337d-4c01-8260-02f13a32f2fd/BonusChapter-23.pdf
+# http://polisci.msu.edu/jacoby/uic/lattice/UIC%20Workshop,%20lattice%20%20Outline.pdf
+# 
 library(lattice)
 eq<-function(x) {x^2}
 x<-1:20
