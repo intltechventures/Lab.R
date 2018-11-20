d2b390a6be6a136548a47b60a5df59499150eb2d
Kelvin D. Meeks <kmeeks@intltechventures.com>
2018-11-19 17:52:24 -0800

    initial commit

---
+++ src/XGBoost/demo.R
@@ -0,0 +1,17 @@
+# https://xgboost.readthedocs.io/en/latest/R-package/xgboostPresentation.html
+
+
+# install.packages("xgboost")
+require(xgboost) 
+
+library(xgboost)
+# load data
+data(agaricus.train, package = 'xgboost')
+data(agaricus.test, package = 'xgboost')
+train <- agaricus.train
+test <- agaricus.test
+# fit model
+bst <- xgboost(data = train$data, label = train$label, max_depth = 2, eta = 1, nrounds = 2,
+               nthread = 2, objective = "binary:logistic", verbose = 2)
+# predict
+pred <- predict(bst, test$data)
