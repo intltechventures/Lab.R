e073168afc1bc7b214f9893d3f8e3721aa5591a6
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-02-27 16:42:24 -0800

    initial commit

---
+++ src/Snippets/Demo.DataTypes.R
@@ -0,0 +1,21 @@
+
+# Vectors
+a <- c(1,2,3,4,5)                             # numeric vector
+a
+summary(a)
+class(a)
+
+b <- c("Tom", "Dick", "Harry", "Sally")       # character vector 
+b
+summary(b)
+class(b)
+
+c <- c(TRUE, FALSE, TRUE, TRUE, FALSE, FALSE) # logical vector 
+c
+summary(c)
+class(c)
+
+
+
+
+
