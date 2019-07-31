7d2d74a18fa79785f7f89e91167ba47ea8cd5b33
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-07-31 01:07:59 -0700

    minor tweaks

--- src/Pie3D/demo_pie3D.r
+++ src/Pie3D/demo_pie3D.r
@@ -1,6 +1,12 @@
-# Simple 3D Pie Chart
+# Demo Simple Plotrix 3D Pie Chart
+# Additional References
+# https://www.rdocumentation.org/packages/plotrix/versions/3.7-6
+# https://www.rdocumentation.org/packages/plotrix/versions/3.7-6/topics/pie3D
+
 library(plotrix)
 slices <- c(33, 33,33)
 lbls <- c("Speed", "Efficiency", "Security")
 colors = c("green", "yellow", "violet")
-pie3D(slices, explode=0.75, labels = lbls, main="Pie Chart of new EA focus", col=colors)
+pie3D(slices, explode=0.15, height=0.45, shade=1.0, theta=pi/6, radius=0.75, labels = lbls, main="New EA Mission Focus", col=colors)
+
+
