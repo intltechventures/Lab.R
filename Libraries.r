8b936743b7f9df3c9300304228f6b6fa5b0933b4
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-08-05 12:05:34 -0700

    initial commit

---
+++ Libraries.r
@@ -0,0 +1,19 @@
+
+# Installation commands for some Useful Libraries
+
+
+# **********************************************************
+# Visualization
+#
+
+
+# https://rdrr.io/cran/plotrix
+install.packages("plotrix")
+
+
+# https://www.r-pkg.org/pkg/klaR
+# https://github.com/cran/klaR
+install.packages("klaR") 
+
+
+
